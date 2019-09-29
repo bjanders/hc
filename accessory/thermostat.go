73d921752a6d4946817ca5bc2d851223dc8404c0
Bjorn Andersson <bjorn@iki.fi>
2019-09-30 00:13:12 +0300

    go fmt on thermostat.go

--- accessory/thermostat.go
+++ accessory/thermostat.go
@@ -13,7 +13,7 @@ type Thermostat struct {
 // NewThermostat returns a Thermostat which implements model.Thermostat.
 //func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp, setTHCS, minTHCS, maxTHCS, stepsTHCS float64) *Thermostat {
 //func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64)  *Thermostat {
-func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64)  *Thermostat {
+func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64) *Thermostat {
 	acc := Thermostat{}
 	acc.Accessory = New(info, TypeThermostat)
 	acc.Thermostat = service.NewThermostat()
@@ -28,10 +28,10 @@ func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64)  *Therm
 	acc.Thermostat.TargetTemperature.SetMaxValue(maxTemp)
 	acc.Thermostat.TargetTemperature.SetStepValue(stepsTemp)
 
-//	acc.Thermostat.TargetTempearture.SetValue(temp)
-//	acc.Thermostat.TargetHeatingCoolingState.SetMinValue(minTemp)
-//	acc.Thermostat.TargetHeatingCoolingState.SetMaxValue(maxTemp)
-//	acc.Thermostat.TargetHeatingCoolingState.SetStepValue(stepsTemp)
+	//	acc.Thermostat.TargetTempearture.SetValue(temp)
+	//	acc.Thermostat.TargetHeatingCoolingState.SetMinValue(minTemp)
+	//	acc.Thermostat.TargetHeatingCoolingState.SetMaxValue(maxTemp)
+	//	acc.Thermostat.TargetHeatingCoolingState.SetStepValue(stepsTemp)
 
 	acc.AddService(acc.Thermostat.Service)
 
