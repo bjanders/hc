5275dcc2479f4c0ff0b4f1a02a85211f47380135
Bjorn Andersson <bjorn@iki.fi>
2019-09-29 23:11:38 +0300

    Reset thermostat API to upstream version

--- accessory/thermostat.go
+++ accessory/thermostat.go
@@ -11,20 +11,27 @@ type Thermostat struct {
 }
 
 // NewThermostat returns a Thermostat which implements model.Thermostat.
-func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64, setTHCS, minTHCS, maxTHCS, stepsTHCS int) *Thermostat {
+//func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp, setTHCS, minTHCS, maxTHCS, stepsTHCS float64) *Thermostat {
+//func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64)  *Thermostat {
+func NewThermostat(info Info, temp, minTemp, maxTemp, stepsTemp float64)  *Thermostat {
 	acc := Thermostat{}
 	acc.Accessory = New(info, TypeThermostat)
 	acc.Thermostat = service.NewThermostat()
+
+	acc.Thermostat.CurrentTemperature.SetValue(temp)
+	acc.Thermostat.CurrentTemperature.SetMinValue(minTemp)
+	acc.Thermostat.CurrentTemperature.SetMaxValue(maxTemp)
+	acc.Thermostat.CurrentTemperature.SetStepValue(stepsTemp)
 
 	acc.Thermostat.TargetTemperature.SetValue(temp)
 	acc.Thermostat.TargetTemperature.SetMinValue(minTemp)
 	acc.Thermostat.TargetTemperature.SetMaxValue(maxTemp)
 	acc.Thermostat.TargetTemperature.SetStepValue(stepsTemp)
 
-	acc.Thermostat.TargetHeatingCoolingState.SetValue(setTHCS)
-	acc.Thermostat.TargetHeatingCoolingState.SetMinValue(minTHCS)
-	acc.Thermostat.TargetHeatingCoolingState.SetMaxValue(maxTHCS)
-	acc.Thermostat.TargetHeatingCoolingState.SetStepValue(stepsTHCS)
+//	acc.Thermostat.TargetTempearture.SetValue(temp)
+//	acc.Thermostat.TargetHeatingCoolingState.SetMinValue(minTemp)
+//	acc.Thermostat.TargetHeatingCoolingState.SetMaxValue(maxTemp)
+//	acc.Thermostat.TargetHeatingCoolingState.SetStepValue(stepsTemp)
 
 	acc.AddService(acc.Thermostat.Service)
 
